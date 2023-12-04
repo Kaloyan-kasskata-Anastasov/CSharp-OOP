a4dcb0151f6ce5fb1d323cffb7d5f8d09c3e5f3d
kasskata <kassskata@gmail.com>
2023-12-04 22:53:40 +0200

    Added Live Solution

---
+++ 12.Workshop-SimpleSnake-LiveSolution/SimpleSnake/GameObjects/Food/Food.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleSnake.GameObjects.Food
+{
+internal class Food : GameObject
+{
+    public Food(char drawSymbol, int points)
+        : base(drawSymbol)
+    {
+        Points = points;
+    }
+
+    public int Points { get; private set; }
+}
+}
---
+++ 12.Workshop-SimpleSnake-LiveSolution/SimpleSnake/GameObjects/Food/FoodHash.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleSnake.GameObjects.Food
+{
+internal class FoodHash : Food
+{
+    private const char Symbol = '#';
+    private const int HashPoints = 3;
+
+    public FoodHash()
+        : base(Symbol, HashPoints)
+    {
+    }
+}
+}
